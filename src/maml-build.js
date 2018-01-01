7e51cef3ad581886f9ddd5161440fe1359f12b4c
Tyler Long <tyler4long@gmail.com>
2018-01-01 19:50:19 +0800

    Support watch mode

--- src/maml-build.js
+++ src/maml-build.js
@@ -1,6 +1,7 @@
 const program = require('commander')
 const path = require('path')
 const { exec } = require('child_process')
+const fs = require('fs')
 
 const { buildPages } = require('./pages')
 
@@ -8,12 +9,26 @@ program
   .version(require('../package.json').version)
   .option('-i, --input <input>', 'specify input directory')
   .option('-o, --output <output>', 'specify output directory')
+  .option('-w, --watch', 'turn on watch mode')
   .parse(process.argv)
 
 const input = program.input || '.'
 const output = program.output || 'dist'
 
-buildPages(input, output)
+const build = () => {
+  buildPages(input, output)
+  // copy assets
+  exec(`cp -r ${path.join(input, 'assets', '*')} ${output}`)
+}
 
-// copy assets
-exec(`cp -r ${path.join(input, 'assets', '*')} ${output}`)
+build()
+
+if (program.watch) { // watch mode
+  const outputPath = fs.realpathSync(output)
+  fs.watch(input, { recursive: true }, (eventType, filename) => {
+    const filePath = fs.realpathSync(filename)
+    if (!filePath.startsWith(outputPath)) {
+      build()
+    }
+  })
+}
